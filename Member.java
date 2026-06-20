5e0890e6a869bb7d08a2a6edb071641fd0b1c26c
vito <aymane.elkhadraoui1@gmail.com>
2026-06-20 03:09:10 +0100

    start working on the Gym manager, first making atributs with capsulation, and constructor that initialize all fields

---
+++ Member.java
@@ -0,0 +1,23 @@
+class Member {
+	private int id;
+
+
+
+
+	private String name;
+	private int age;
+	private int weight;
+	private int height;
+
+	public Member (int id, String name, int age, int weight, int height) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		this.height = height;
+	}
+
+	// public int getId(){ return this.id }
+
+	// public String getN
+}
